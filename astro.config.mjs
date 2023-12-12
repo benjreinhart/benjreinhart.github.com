852e9b83e83c0bcd31703c65f4ac1412597e36c6
Ben Reinhart <benjreinhart@gmail.com>
2023-12-11 20:44:47 -0800

    First pass at blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://benreinhart.com',
+  integrations: [tailwind(), mdx()],
 });
